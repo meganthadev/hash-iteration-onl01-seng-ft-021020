e31d2f7ebe999947e25be5c1dadca5cd9616c320
Megan Horsfield <meganthadev@gmail.com>
2020-02-22 07:42:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each { |kids_name, age| puts "Happy Birthday #{kids_namename}! You are now #{age} years old!" }
+  birthday_kids.each { |kids_name, age| puts "Happy Birthday #{kids_name}! You are now #{age} years old!" }
 end
 
 
