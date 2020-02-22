75857e8bd5a91c435911dc9eeec3c5171a19208b
Megan Horsfield <meganthadev@gmail.com>
2020-02-22 07:46:43 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,4 +8,11 @@ def happy_birthday(birthday_kids)
   birthday_kids.each { |kids_name, age| puts "Happy Birthday #{kids_name}! You are now #{age} years old!" }
 end
 
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age| 
+    if age <= 12
+      puts happy_birthday
+    end
+end    
+
 
